a933f61ccacf40dfd63d14a1765a4ff36388f0ca
theoreticalbts <theoreticalbts@users.noreply.github.com>
2017-07-31 16:33:40 -0400

    Delete obsolete documents #12

--- build-pdf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Prereqs for building on Ubuntu LTS:  sudo apt-get install pandoc texlive-latex-base texlive-fonts-recommended texlive-latex-recommended
-pandoc -o marketmaker.pdf marketmaker.md
